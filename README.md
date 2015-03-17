d226b1711eab9ab3c2a84e37d281b2c989631d19
Steve Mosley <me@steve-mosley.com>
2015-03-17 18:26:48 +1300

    Working Downloader with resume

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ WORK IN PROGRESS
 Simple tool for picking files off an FTP site.
 
 
-TODO  
-- [] Sync Folders
+TODO
+- [*] Sync Folders
+- [] Only download files once
 - [] Keep running on a schedule
 - [] Filter files based on Regex (reject files)
 - [] Notify when Files/Folders are completed
--- src/main/java/com/smozely/ftpgrabber/FtpSync.java
+++ src/main/java/com/smozely/ftpgrabber/FtpSync.java
@@ -1,8 +1,6 @@
 package com.smozely.ftpgrabber;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,64 +40,90 @@ public class FtpSync {
         if (!this.toDir.exists()) {
             this.toDir.mkdirs();
         }
-
     }
 
     public void run() {
-        log.info("Running Sync : {}", host);
+        log.info("Sync Started");
         FTPClient client = new FTPClient();
+
         try {
             client.connect(host);
-            client.login(username, password);
 
-            // TODO Check we are really logged in
-            System.out.println(client.getReplyString());
+            int replyCode = client.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                client.disconnect();
+                log.error("Failed to connect to {}", host);
+                return;
+            } else {
+                log.info("Connected to {}", host);
+            }
+
+            boolean loggedIn = client.login(username, password);
+            if (!loggedIn) {
+                log.error("Failed to login to {}, with user {}", host, username);
+                return;
+            }
+
             client.setFileType(FTP.BINARY_FILE_TYPE);
+            client.changeWorkingDirectory(fromDir);
 
-            List<FTPFile> files = Arrays.asList(client.listFiles(fromDir));
-
-            files.forEach(file -> {
-                try {
-                    log.info("Processing [{}]", file.getName());
-                    if (file.isDirectory()) {
-                        processFolder(client, file);
-                    } else {
-                        copyFileToLocal(client, file);
-                    }
-                } catch (IOException e) {
-
-                    e.printStackTrace();
-                }
-            });
+            log.info("Connected and Logged in Starting file processing");
+            processFolder(client, toDir);
 
             client.logout();
         } catch (IOException e) {
             log.error("Sync Failed", e);
+        } finally {
+            if(client.isConnected()) {
+                try {
+                    client.disconnect();
+                } catch(IOException ioe) {
+                    // NOOP
+                }
+            }
         }
+
+        log.info("Sync Finished");
     }
 
-    private void processFolder(FTPClient client, FTPFile file) throws IOException {
-        List<FTPFile> files = Arrays.asList(client.listFiles(fromDir + file.getName()));
+    private void processFolder(FTPClient client, File outFolder) throws IOException {
+        List<FTPFile> files = Arrays.asList(client.listFiles());
 
-//        files.forEach(file -> {
-
-
-//        });
+        files.forEach(file -> {
+            try {
+                log.info("Processing [{}]", file.getName());
+                if (file.isDirectory()) {
+                    client.changeWorkingDirectory(file.getName());
+                    File nextFolder = new File(outFolder, file.getName());
+                    if (!nextFolder.exists()) {
+                        nextFolder.mkdirs();
+                    }
+                    processFolder(client, nextFolder);
+                    client.changeToParentDirectory();
+                } else {
+                    copyFileToLocal(client, outFolder, file);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    private void copyFileToLocal(FTPClient client, FTPFile file) throws IOException {
-        File outFile = new File(toDir, file.getName());
+    private void copyFileToLocal(FTPClient client, File currentDir, FTPFile file) throws IOException {
+
+        File outFile = new File(currentDir, file.getName());
         if (!outFile.exists()) {
             log.info("About to download [{}] with Size [{}]", file.getName(), file.getSize());
-            File tempFile = new File(toDir, file.getName() + ".writing");
+            File tempFile = new File(currentDir, file.getName() + ".writing");
 
             if (tempFile.exists()) {
                 long fileLength = tempFile.length();
+                log.info("Resuming download of [{}] from [{}]", file.getName(), fileLength);
                 client.setRestartOffset(fileLength);
             }
 
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(tempFile, true));
-            client.retrieveFile(fromDir + "/" + file.getName(), out);
+            client.retrieveFile(file.getName(), out);
             out.close();
             log.info("File received [{}]", file.getName());
 
